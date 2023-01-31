b7bab9a56b58a211b2d77f1faebb14f39f7d706b
PabloArdila1 <109166175+PabloArdila1@users.noreply.github.com>
2023-01-31 18:10:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,179 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 200;
-
-let scaleRatio = null; 
-let previousTime = null;
-
-function setScreen(){
-
-    scaleRatio = getScaleRatio();
-    canvas.width = GAME_WIDTH * scaleRatio;
-    canvas.height =GAME_HEIGHT * scaleRatio;
-}
-
-setScreen();
-
-window.addEventListener('resize', setScreen);
-
-//this functios fits de game on any size of screens
-function getScaleRatio() {
-    const screenHeight = Math.min(window.innerHeight, document.documentElement.clientHeight);
-
-    const screenWidth = Math.min(window.innerWidth, document.documentElement.clientWidth);
-
-    //window is wider than the game width
-    if (screenWidth / screenHeight < GAME_WIDTH / GAME_HEIGHT) {
-        return screenWidth / GAME_WIDTH;
-    } else {
-        return screenHeight / GAME_HEIGHT;
+class SnakePart{
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
-function clearScreen() {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+let speed = 7;
 
-function gameLoop(currentTime) {
-    // console.log(currentTime)
+//grid
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+
+//snake
+let headX = 10;
+let headY = 10;
+const snakeParts = [];
+let tailLength = 2; 
+
+
+//apple object
+let appleX = 5;
+let appleY = 5;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
+let score = 0;
+
+const gulpSound = new Audio("gulp.mp3");
+
+//Game loop 
+function drawGame(){
+    changeSnakePosition();
+let result = isGameOver();
+    if(result){
+        return;
+    }
+
     clearScreen();
-    requestAnimationFrame(gameLoop);
+
+    checkAppleCollision();
+    drawApple();
+    drawSnake();
+
+    drawScore();
+    if (score >= 5) {
+        speed = 11;
+    } else if (score >= 10) {
+        speed = 15;
+    } else if (score >= 25) {
+        speed = 30;
+    } 
+
+
+    setTimeout(drawGame, 1000/ speed);
 }
 
-requestAnimationFrame(gameLoop);
+function isGameOver(){
+    let gameOver = false;
+
+    if (yVelocity === 0 && xVelocity === 0) {
+        return false;
+    }
+
+    //collition with walls
+    if (headX < 0 || headY < 0) {
+        gameOver = true; 
+    } else if (headX === tileCount || headY === tileCount) {
+        gameOver = true; 
+    }
+
+    for(let i = 0; i < snakeParts.length; i++){
+        let part = snakeParts[i];
+        if (part.x === headX && part.y === headY) {
+            gameOver = true; 
+            break;
+        }
+    }
+
+    if (gameOver) {
+        ctx.fillStyle = 'orange';
+        ctx.font = "50px Verdana";
+
+        ctx.fillText('Game Over!', canvas.width / 6.5, canvas.height / 2)
+    }
+
+    return gameOver
+}
+
+function drawScore(){
+    ctx.fillStyle = 'white';
+    ctx.font = '15px  verdana';
+    ctx.fillText('Score ' + score, canvas.width-70, 16)
+}
+
+function clearScreen() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+//this draw the sanke 
+function drawSnake() {
+    ctx.fillStyle = 'green';
+    for(let i = 0; i < snakeParts.length; i++) {
+        let part = snakeParts[i];
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
+    }
+
+    snakeParts.push(new SnakePart(headX, headY)); //put an item at the end of the list next to the head
+    while (snakeParts.length > tailLength) { //remove the furthers item from the snake parts if have more than  our tail size.  
+        snakeParts.shift(); 
+    }
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize)
+    
+}
+
+function changeSnakePosition(){
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+function drawApple(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize)
+}
+
+function checkAppleCollision() {
+    if (appleX === headX && appleY === headY) {
+          appleX = Math.floor(Math.random() * tileCount)
+          appleY = Math.floor(Math.random() * tileCount)
+          tailLength++; 
+          score++;
+          gulpSound.play();
+    }
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+    //up 
+    if (event.keyCode == 38) {
+        if (yVelocity == 1)
+        return;
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+
+    //down
+    if (event.keyCode == 40) {
+        if (yVelocity == -1)
+        return;
+        yVelocity = 1;
+        xVelocity = 0;
+    }
+
+    //left
+    if (event.keyCode == 37) {
+        if (xVelocity == 1)
+        return;
+        yVelocity = 0;
+        xVelocity = -1;
+    }
+
+    //right
+    if (event.keyCode == 39) {
+        if (xVelocity == -1)
+        return;
+        yVelocity = 0;
+        xVelocity = 1;
+    }
+}
+
+drawGame();
